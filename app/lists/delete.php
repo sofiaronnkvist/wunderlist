3b3a4ce56deb34fd27ce9665356e2a9a6151dfa7
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2021-12-27 12:40:47 +0100

    Added file structure for tasks and lists.

--- app/lists/delete.php
+++ app/lists/delete.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-// In this file we delete tasks in the database.
+// In this file we delete lists in the database.
 
 redirect('/');
